96d8831a65311fa1e5eaffd8ea974c8ca7191ab7
Aleksandr Seymor <godseymor@gmail.com>
2018-07-19 23:53:11 +0500

    Полностью реализует задание

--- js/paint.js
+++ js/paint.js
@@ -1,38 +1,73 @@
 'use strict';
 
 (function () {
+
+  var canvasBody = document.querySelector('.canvas');
   var paintField = document.querySelector('#paintField');
   var canvasSettings = document.querySelector('.canvas__settings');
+
+  var canvasClear = document.querySelector('.canvas__clear');
+  var canvasSave = document.querySelector('.canvas__save');
+
+  document.querySelector('.destroy').addEventListener('click', function () {
+    localStorage.clear();
+  })
+
+  var ctx = paintField.getContext('2d');
+  ctx.lineCap = 'round';
+  var isDraw;
 
   function setBrushStyle() {
     ctx.strokeStyle = canvasSettings.colorStyle.value;
     ctx.lineWidth = canvasSettings.brushStyle.value;
-  }
-  canvasSettings.addEventListener('change', setBrushStyle);
 
-  var ctx = paintField.getContext('2d');
-  var isPainting = false;
+    document.body.style.borderColor = canvasSettings.colorStyle.value;
+  }
 
   function onMouseDown(evt) {
-    isPainting = true;
+    isDraw = true;
     ctx.beginPath();
     ctx.moveTo(evt.pageX - paintField.offsetLeft, evt.pageY - paintField.offsetTop);
   }
 
   function onMouseMove(evt) {
-    if (isPainting) {
+    if (isDraw) {
       ctx.lineTo(evt.pageX - paintField.offsetLeft, evt.pageY - paintField.offsetTop);
       ctx.stroke();
     }
   }
 
   function onMouseUp() {
-    isPainting = false;
+    isDraw = false;
     ctx.closePath();
   }
 
+  function onCanvasClearButtonClick() {
+    ctx.clearRect(0, 0, 300, 150);
+  }
+
+  function onCanvasSaveButtonClick() {
+    var dataUrl = paintField.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, "");
+    localStorage.setItem('img', dataUrl);
+  }
+
+  window.addEventListener('storage', function(evt) {
+    console.log(evt);
+  });
+
   paintField.addEventListener('mousedown', onMouseDown);
   paintField.addEventListener('mousemove', onMouseMove);
-  paintField.addEventListener('mouseup', onMouseUp);
+  canvasBody.addEventListener('mouseup', onMouseUp);
+
+  canvasSettings.addEventListener('change', setBrushStyle);
+
+  canvasClear.addEventListener('click', onCanvasClearButtonClick);
+  canvasSave.addEventListener('click', onCanvasSaveButtonClick);
+
+  document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.length > 0) {
+      ctx.drawImage(img, 0, 0);
+    }
+  });
 
 })();
